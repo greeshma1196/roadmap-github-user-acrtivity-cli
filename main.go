776924939ca352fc70f0d139797c092ce3ea3165
Greeshma Ann Mathew <greeshma.mathew@gmail.com>
2024-11-27 14:18:32 +0100

    implemented parsing for CreateEvent

--- main.go
+++ main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
+
+type Event []struct {
+	Type string `json:"type"`
+	Repo struct {
+		Name string `json:"name"`
+	} `json:"repo"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+type CreateEvent struct {
+	RefType string `json:"ref_type"`
+}
 
 func checkStatusCode(statusCode int) error {
 
 	if statusCode == 200 || statusCode == 304 {
 		fmt.Printf("Success\n")
 	} else if statusCode == 403 {
-		return fmt.Errorf("Forbidden\n")
+		return fmt.Errorf("forbidden")
 	} else if statusCode == 404 {
-		return fmt.Errorf("Not Found\n")
+		return fmt.Errorf("not found")
 	} else if statusCode == 503 {
-		return fmt.Errorf("Service Unavailable\n")
+		return fmt.Errorf("service unavailable")
 	}
+	return nil
+}
+
+func parseCreateEvent(payload json.RawMessage, reponame string) error {
+	var cresp CreateEvent
+	if err := json.Unmarshal(payload, &cresp); err != nil {
+		panic(err)
+	}
+
+	if cresp.RefType == "repository" {
+		fmt.Printf("Created new repository %s\n", reponame)
+	} else if cresp.RefType == "branch" {
+		fmt.Printf("Created new branch %s\n", reponame)
+	} else if cresp.RefType == "tag" {
+		fmt.Printf("Created new tag %s\n", reponame)
+	} else {
+		return fmt.Errorf("unable to parse, reference type is empty")
+	}
+
 	return nil
 }
 
@@ -36,6 +68,30 @@ func main() {
 
 	if err != nil {
 		panic(err)
+	}
+
+	var cresp Event
+	if err := json.NewDecoder(resp.Body).Decode(&cresp); err != nil {
+		panic(err)
+	}
+
+	for _, event := range cresp {
+		if event.Type == "CreateEvent" {
+			err := parseCreateEvent(event.Payload, event.Repo.Name)
+			if err != nil {
+				panic(err)
+			}
+		} else if event.Type == "DeleteEvent" {
+			fmt.Printf("DeleteEvent: %s\n", event.Repo.Name)
+		} else if event.Type == "IssuesEvent" {
+			fmt.Printf("IssuesEvent: %s\n", event.Repo.Name)
+		} else if event.Type == "PullRequestEvent" {
+			fmt.Printf("PullRequestEvent: %s\n", event.Repo.Name)
+		} else if event.Type == "PushEvent" {
+			fmt.Printf("PushEvent: %s\n", event.Repo.Name)
+		} else if event.Type == "ReleaseEvent" {
+			fmt.Printf("ReleaseEvent: %s\n", event.Repo.Name)
+		}
 	}
 
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,18 +17,48 @@ func TestCheckStatusCode(t *testing.T) {
 	t.Run("Successfully validates forbidden status code", func(t *testing.T) {
 		statusCode := 403
 		err := checkStatusCode(statusCode)
-		require.EqualError(t, err, "Forbidden\n")
+		require.EqualError(t, err, "forbidden")
 	})
 
 	t.Run("Successfully validates not found status code", func(t *testing.T) {
 		statusCode := 404
 		err := checkStatusCode(statusCode)
-		require.EqualError(t, err, "Not Found\n")
+		require.EqualError(t, err, "not found")
 	})
 
 	t.Run("Successfully validates service unavailable status code", func(t *testing.T) {
 		statusCode := 503
 		err := checkStatusCode(statusCode)
-		require.EqualError(t, err, "Service Unavailable\n")
+		require.EqualError(t, err, "service unavailable")
+	})
+}
+
+func TestParseCreateEvent(t *testing.T) {
+	t.Run("Successfully validates CreateEvent for repository", func(t *testing.T) {
+		payload := `{"ref_type": "repository"}`
+		reponame := "sample_repo"
+		err := parseCreateEvent(json.RawMessage(payload), reponame)
+		require.Nil(t, err)
+	})
+
+	t.Run("Successfully validates CreateEvent for branch", func(t *testing.T) {
+		payload := `{"ref_type": "branch"}`
+		reponame := "sample_repo"
+		err := parseCreateEvent(json.RawMessage(payload), reponame)
+		require.Nil(t, err)
+	})
+
+	t.Run("Successfully validates CreateEvent for tag", func(t *testing.T) {
+		payload := `{"ref_type": "tag"}`
+		reponame := "sample_repo"
+		err := parseCreateEvent(json.RawMessage(payload), reponame)
+		require.Nil(t, err)
+	})
+
+	t.Run("Successfully validates error for CreateEvent", func(t *testing.T) {
+		payload := `{"ref_type": ""}`
+		reponame := "sample_repo"
+		err := parseCreateEvent(json.RawMessage(payload), reponame)
+		require.EqualError(t, err, "unable to parse, reference type is empty")
 	})
 }
